e5163e81f4c009ebcb352555a4b6211bb3aab741
craisin <kahkaicyap@gmail.com>
2026-08-27 09:54:58 -0700

    config stuff

--- train.py
+++ train.py
@@ -3,34 +3,44 @@ import torch
 import torch.nn
 import torch.nn.functional as F
 from transformers import AutoTokenizer
+from types import SimpleNamespace
 
 from model import Model
 from data import TextDataset
 
 logger = logging.getLogger(__name__)
 
-def train():
+
+def train(config=None, **kwargs):
 
     assert torch.cuda.is_available(), "CUDA GPU required for training."
     device = "cuda"
 
-    context_len = 1024
-    batch_size = 8
-    grad_accum_steps = 4
-    epochs = 1
-
-    logging_rate = 200
+    config_defaults = {
+        "context": 1024,
+        "epochs": 1,
+        "grad_steps": 4,
+        "microbatch_size": 8,
+        "logging_rate": 20,
+    }
+    if config is None:
+        config = kwargs
+    config = SimpleNamespace(**(config_defaults | config))
 
     tokenizer = AutoTokenizer.from_pretrained("gpt2")
     raw_dataset = load_dataset("wikitext", "wikitext-103-raw-v1", split="train")
-    dataset = TextDataset(raw_dataset, tokenizer, seq_len=context_len)
+    dataset = TextDataset(raw_dataset, tokenizer, seq_len=config.context)
     dataloader = DataLoader(
-        dataset, batch_size=batch_size, shuffle=True, num_workers=2, pin_memory=True
+        dataset,
+        batch_size=config.microbatch_size,
+        shuffle=True,
+        num_workers=2,
+        pin_memory=True,
     )
 
     model = Model(
         vocab_size=tokenizer.vocab_size,
-        max_context=context_len,
+        max_context=config.context,
     ).to(device)
     model = torch.compile(model)
 
@@ -46,7 +56,7 @@ def train():
 
     model.train()
 
-    for epoch in range(epochs):
+    for epoch in range(config.epochs):
         optimizer.zero_grad(set_to_none=True)
         accum_loss = 0
         for step, (x, y) in enumerate(dataloader):
@@ -54,23 +64,22 @@ def train():
             with torch.amp.autocast(device_type="cuda", dtype=torch.bfloat16):
                 logits = model(x)
                 loss = criterion(logits.view(-1, logits.size(-1)), y.view(-1))
-                loss = loss / grad_accum_steps
+                loss = loss / config.grad_steps
             accum_loss += loss.item()
             loss.backward()
 
-            if step % grad_accum_steps == grad_accum_steps - 1:
+            if step % config.grad_steps == config.grad_steps - 1:
                 # TODO: gradient clipping
                 # TODO: variable learning
                 optimizer.step()
-            
-            if step % (logging_rate * grad_accum_steps) == grad_accum_steps - 1:
+
+            if (
+                step % (config.logging_rate * config.grad_steps)
+                == config.grad_steps - 1
+            ):
                 logger.info(f"Step {step} | Loss {accum_loss} ")
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO,
-        filename="train.log",
-        filemode="a"
-    )
+    logging.basicConfig(level=logging.INFO, filename="train.log", filemode="a")
     train()
