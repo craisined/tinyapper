a903cbdb03c1c3cf467ab872a8ed04697ca8fa9c
craisin <kahkaicyap@gmail.com>
2026-09-12 23:45:46 -0700

    client side socket

--- demos/tinyapper/static/script.js
+++ demos/tinyapper/static/script.js
@@ -1,5 +1,13 @@
-function handleForm (e){
-    e.preventDefault();
-    alert("submitted")
-}
+const socket = io();
 
+function handleForm(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+    socket.emit("chat", {prompt: data.prompt})
+}
+document.querySelector(".input-container").addEventListener(handleForm);
+
+socket.on("output", (data) => {
+    console.log("The server sent some news:", data);
+});
